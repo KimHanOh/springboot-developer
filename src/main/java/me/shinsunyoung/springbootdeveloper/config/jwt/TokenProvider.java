b2cd914fbabcd49ed40968334a994e590c6e096b
KimHanOh <loj7605@naver.com>
2025-08-12 12:05:52 +0900

    project init

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/config/jwt/TokenProvider.java
@@ -0,0 +1,25 @@
+package me.shinsunyoung.springbootdeveloper.config.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Service
+public class TokenProvider {
+    private final JwtProperties jwtProperties;
+
+    public String generateToken(User user, Duration expiredAt) {
+        Date now = new Date();
+        return makeToken(new Date(now.getTime() + expiredAt.toMillis()), user);
+    }
+
+    private String makeToken(Date expiry, User user) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .set
+    }
+}
